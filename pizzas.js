091cdfba672a3cbb6a3cb848ba430939ef47fdf9
dsgils <129603169+dsgils@users.noreply.github.com>
2023-06-10 19:30:54 -0300

    teste

---
+++ pizzas.js
@@ -0,0 +1,6 @@
+let choices = ['a', 'b', 'c']
+let tamanho_array = choices.length
+let n_aleatorio = Math.floor(Math.random() * tamanho_array);
+let retorno_aleatorio = choices[n_aleatorio];
+
+console.log('retono_aleatorio')
